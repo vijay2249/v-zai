553052b0d5540c383dfb44188db94d0f0254ec78
vijay chowdary <vijaychowdary3699@gmail.com>
2021-09-19 10:02:50 +0530

    codepen addition

--- extension.js
+++ extension.js
@@ -6,33 +6,34 @@ const xmlParse = require('fast-xml-parser');
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-	const blogs = await axios.get("https://blog.webdevsimplified.com/rss.xml");
-	const articles = xmlParse.parse(blogs.data).rss.channel.item.map(article => {
-		return {
+	let web_dev_articles = []
+	const wds_blogs = await axios.get("https://blog.webdevsimplified.com/rss.xml");
+	xmlParse.parse(wds_blogs.data).rss.channel.item.map(article => {
+		return web_dev_articles.push({
 			label: article.title,
 			detail: article.description,
 			link: article.link,
-		}
+		})
 	})
-	console.log(articles)
-	const actionsAvailable = [{"label": 'github users'}, {"label":"wds blog post", "clicked":false}]
-
+	const codePen_blogs = await axios.get("https://blog.codepen.io/feed/");
+	xmlParse.parse(codePen_blogs.data).rss.channel.item.map(article =>{
+		return web_dev_articles.push({
+			label: article.title,
+			detail: article.description,
+			link: article.link
+		}) 
+	})
+	console.log(web_dev_articles[0])
 	console.log('Congratulations, "v-zai-extension" is now active!');
-	let disposable = vscode.commands.registerCommand('v-zai-extension.v-zai', async function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('v-zai-extension.v-zai', async () => {
 		var time = new Date()
 		let displayDate = time.toLocaleDateString()
 		let displayTime = time.toLocaleTimeString()
-		let displayMsg = `Date: ${displayDate}\nTime: ${displayTime}`
-		vscode.window.showInformationMessage(displayMsg);
-		
-		// const searchBoxDisplay = vscode.window.showQuickPick(actionsAvailable, {matchOnDetail:true})
-		// if(searchBoxDisplay == null)return
-		
-		const article = await vscode.window.showQuickPick(articles,{matchOnDetail:true})
+		vscode.window.showInformationMessage(`Date: ${displayDate}\tTime: ${displayTime}`);
+				
+		const article = await vscode.window.showQuickPick(web_dev_articles,{matchOnDetail:true})
 		if(article == null)return
 		vscode.env.openExternal(article.link)
-
 	});
 
 	context.subscriptions.push(disposable);
